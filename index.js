831e97b168349e0a775334177633dddf87c7d305
MuchinaWamburia <lucymuchina20@gmail.com>
2022-11-23 10:12:17 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
-
-// Write your code here
+{
+    let num1 =31
+    let num2 =2
+    let multiply=num1*num2;
+}
+{
+    let Number =Math.floor( Math.random() * 100);
+    let random=Number
+}   
+{
+    let num3= 59
+    let num4=5
+    let mod =num3%num4
+}
+{
+    let num=Math.max(16,17,18,19,20)
+    let max =num
+}
